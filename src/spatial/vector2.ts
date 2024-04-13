351a516f44e18eb28404c975989be3d5b289c706
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-04-13 09:17:46 -0300

    feat: adding a bidimensional vector

---
+++ src/spatial/vector2.ts
@@ -0,0 +1,43 @@
+import { VectorN } from "./vectorN.js";
+
+/**
+ * The `Vector2` class represents a static bidimensional vector.
+ * 
+ * @version 0.3.0
+ * @author Daniel de Oliveira <oliveira.daaaaniel@gmail.com>
+ */
+export class Vector2 extends VectorN {
+
+  /**
+   * This constant represents a zero vector (a vector that has its components
+   * equal to `0`).
+   */
+  public static readonly ZERO = new Vector2(0, 0);
+
+  /**
+   * @param x The x component of the vector
+   * @param y The y component of the vector
+   */
+  constructor(x: number, y: number) {
+    super(x, y);
+  }
+
+  public setX(x: number) {
+    return this.setComponent(0, x);
+  }
+
+  public getX(): number {
+    return this.getComponent(0);
+  }
+
+  public setY(y: number) {
+    return this.setComponent(1, y);
+  }
+
+  public getY(): number {
+    return this.getComponent(1);
+  }
+
+}
+
+export default { Vector2 };
