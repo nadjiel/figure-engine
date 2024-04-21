9c770f15f289ec3344d2d86dfc883ad09ba76dec
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-04-21 10:18:04 -0300

    doc: adding description to StageElement

--- src/stage/stageElement.ts
+++ src/stage/stageElement.ts
@@ -1,5 +1,12 @@
 import { GameIterable } from "../main/gameIterable.js";
 
+/**
+ * The `StageElement` interface represents a game iterable element that
+ * is managed by a stage.
+ * 
+ * @version 0.3.0
+ * @author Daniel de Oliveira <oliveira.daaaaniel@gmail.com>
+ */
 export interface StageElement extends GameIterable {
 
 }
