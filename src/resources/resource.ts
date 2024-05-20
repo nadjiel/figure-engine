f3434184a5856acb068ddba740c539511fde145e
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-05-20 13:25:44 -0300

    doc: describing the Resource class

--- src/resources/resource.ts
+++ src/resources/resource.ts
@@ -1,12 +1,31 @@
-
+/**
+ * The `Resource` interface describes the classes that import external resources
+ * with methods to load and get the resource.
+ * 
+ * @version 0.4.0
+ * @author Daniel de Oliveira <oliveira.daaaaniel@gmail.com>
+ */
 export interface Resource<T = HTMLImageElement | HTMLAudioElement> {
 
+  /**
+   * This method is intended to return the imported `Resource`.
+   */
   get(): T;
 
+  /**
+   * This method is intended to return the path from where this `Resource` was
+   * imported.
+   */
   getPath(): string;
 
+  /**
+   * This method is intended to load the external `Resource`.
+   */
   load(): Promise<T>;
 
+  /**
+   * This method is intended to tell if the `Resource` is finished loading.
+   */
   isLoaded(): boolean;
 
 }
