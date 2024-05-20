bb15b255f09813e653d54f4a09541f7b681d54a9
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-05-20 13:01:24 -0300

    doc: describing the ImageResource class

--- src/resources/imageResource.ts
+++ src/resources/imageResource.ts
@@ -1,26 +1,59 @@
 import { Resource } from "./resource.js";
 import { ResourceError } from "../errors/resourceError.js";
 
+/**
+ * The `ImageResource` class allows importing and loading an image from
+ * elsewhere.
+ * 
+ * @version 0.4.0
+ * @author Daniel de Oliveira <oliveira.daaaaniel@gmail.com>
+ */
 export class ImageResource implements Resource<HTMLImageElement> {
 
+  /**
+   * This property stores the image itself in an {@linkcode HTMLImageElement}.
+   */
   private image = new Image();
 
+  /**
+   * This property stores the path to where the image resource is located.
+   */
   private path: string;
 
+  /**
+   * This property indicates if this `ImageResource` is loaded.
+   */
   private loaded = false;
 
+  /**
+   * @param path The path to the image to import.
+   */
   constructor(path: string) {
     this.path = path;
   }
 
+  /**
+   * @returns The image that this `ImageResource` imports.
+   */
   public get(): HTMLImageElement {
     return this.image;
   }
 
+  /**
+   * @returns The path of the image that this `ImageResource` imports.
+   */
   public getPath(): string {
     return this.path;
   }
 
+  /**
+   * Returns a promise that tries to load the resource in the path specified in
+   * this `ImageResource`.
+   * 
+   * If the loading is successful, the promise resolves with the loaded image,
+   * if it fails, it rejects with a {@linkcode ResourceError}.
+   * @returns A promise that tries to load the image.
+   */
   public async load(): Promise<HTMLImageElement> {
     return new Promise((resolve, reject) => {
       this.image.onload = () => {
@@ -35,6 +68,9 @@ export class ImageResource implements Resource<HTMLImageElement> {
     });
   }
 
+  /**
+   * @returns A `boolean` indicating if this `ImageResource` is loaded.
+   */
   public isLoaded(): boolean {
     return this.loaded;
   }
