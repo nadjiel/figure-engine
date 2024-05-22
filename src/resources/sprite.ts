085c72117f25f803cc6acf780a5b720c1e699130
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-05-21 21:30:39 -0300

    code: starting Sprite implementation

--- src/resources/sprite.ts
+++ src/resources/sprite.ts
@@ -1,7 +1,34 @@
 import { ImageResource } from "./imageResource.js";
 import { Vector2 } from "../spatial/vector2.js";
+import { ArgumentError } from "../errors/argumentError.js";
+
+interface BoundingBox {
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+}
 
 export class Sprite {
+
+  private image: ImageResource;
+
+  private columns = 1;
+
+  private rows = 1;
+
+  private margins: BoundingBox = {
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0
+  };
+
+  private gaps: Vector2 = Vector2.createZero();
+
+  private includedFrames: Array<number> = [0];
+
+  private frame: number = 0;
 
   constructor(
     image: ImageResource,
@@ -14,83 +41,141 @@ export class Sprite {
     horizontalGap = 0,
     verticalGap = 0
   ) {
-
+    this.image = image;
   }
 
   public getImage(): HTMLImageElement {
-    return new Image();
+    return this.image.get();
   }
 
   public getWidth(): number {
-    return 0;
+    return this.image.get().width;
   }
 
   public getHeight(): number {
-    return 0;
-  }
-
-  public setRows(amount: number): void {
-
-  }
-
-  public getRows(): number {
-    return 0;
+    return this.image.get().height;
   }
 
   public setColumns(amount: number): void {
+    if(amount < 1) throw new ArgumentError(
+      `Sprite must have 1 or more columns (received ${amount})`
+    );
+    amount = Math.floor(amount);
 
+    this.columns = amount;
   }
 
   public getColumns(): number {
-    return 0;
+    return this.columns;
   }
 
-  public setTopMargin(margin: number): void {
+  public setRows(amount: number): void {
+    if(amount < 1) throw new ArgumentError(
+      `Sprite must have 1 or more rows (received ${amount})`
+    );
+    amount = Math.floor(amount);
 
+    this.rows = amount;
   }
 
-  public getTopMargin(): number {
-    return 0;
-  }
-
-  public setRightMargin(margin: number): void {
-    
-  }
-
-  public getRightMargin(): number {
-    return 0;
+  public getRows(): number {
+    return this.rows;
   }
 
   public setLeftMargin(margin: number): void {
-    
+    if(margin < 0) throw new ArgumentError(
+      `Sprite can't have negative margins (received ${margin})`
+    );
+    margin = Math.floor(margin);
+
+    // Actually this must also take into account the horizontal gaps and right margin
+    if(margin > this.getWidth()) margin = this.getWidth();
+
+    this.margins.left = margin;
   }
 
   public getLeftMargin(): number {
-    return 0;
+    return this.margins.left;
+  }
+
+  public setRightMargin(margin: number): void {
+    if(margin < 0) throw new ArgumentError(
+      `Sprite can't have negative margins (received ${margin})`
+    );
+    margin = Math.floor(margin);
+
+    // Actually this must also take into account the horizontal gaps and left margin
+    if(margin > this.getWidth()) margin = this.getWidth();
+
+    this.margins.right = margin;
+  }
+
+  public getRightMargin(): number {
+    return this.margins.right;
+  }
+
+  public setTopMargin(margin: number): void {
+    if(margin < 0) throw new ArgumentError(
+      `Sprite can't have negative margins (received ${margin})`
+    );
+    margin = Math.floor(margin);
+
+    // Actually this must also take into account the vertical gaps and bottom margin
+    if(margin > this.getHeight()) margin = this.getHeight();
+
+    this.margins.top = margin;
+  }
+
+  public getTopMargin(): number {
+    return this.margins.top;
   }
 
   public setBottomMargin(margin: number): void {
-    
+    if(margin < 0) throw new ArgumentError(
+      `Sprite can't have negative margins (received ${margin})`
+    );
+    margin = Math.floor(margin);
+
+    // Actually this must also take into account the vertical gaps and top margin
+    if(margin > this.getHeight()) margin = this.getHeight();
+
+    this.margins.bottom = margin;
   }
 
   public getBottomMargin(): number {
-    return 0;
+    return this.margins.bottom;
   }
 
   public setHorizontalGap(gap: number): void {
-    
+    if(gap < 0) throw new ArgumentError(
+      `Sprite can't have negative gaps (received ${gap})`
+    );
+    gap = Math.floor(gap);
+
+    // Actually this must also take into account the horizontal gaps and margins
+    if(gap > this.getWidth()) gap = this.getWidth();
+
+    this.gaps.setX(gap);
   }
 
   public getHorizontalGap(): number {
-    return 0;
+    return this.gaps.getX();
   }
 
   public setVerticalGap(gap: number): void {
-    
+    if(gap < 0) throw new ArgumentError(
+      `Sprite can't have negative gaps (received ${gap})`
+    );
+    gap = Math.floor(gap);
+
+    // Actually this must also take into account the vertical gaps and margins
+    if(gap > this.getHeight()) gap = this.getHeight();
+
+    this.gaps.setY(gap);
   }
 
   public getVerticalGap(): number {
-    return 0;
+    return this.gaps.getY();
   }
 
   // Specify the only frames that should be considered
