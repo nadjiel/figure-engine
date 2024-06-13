3196c13c80543442cfb90f40af34e38f625c72f1
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-06-13 13:52:32 -0300

    fix: fixing OneDrawStrategy sprite drawing

--- src/graphical/oneDrawStrategy.ts
+++ src/graphical/oneDrawStrategy.ts
@@ -1,11 +1,12 @@
-import { Sprite } from "../resources/sprite";
-import { Rectangle } from "../spatial/rectangle";
-import { Color } from "./color";
-import { DrawStrategy } from "./drawStrategy";
+import { Sprite } from "../resources/sprite.js";
+import { Rectangle } from "../spatial/rectangle.js";
+import { Vector2 } from "../spatial/vector2.js";
+import { Color } from "./color.js";
+import { DrawStrategy } from "./drawStrategy.js";
 
 export class OneDrawStrategy implements DrawStrategy {
 
-  drawColor(
+  public drawColor(
     ctx: CanvasRenderingContext2D,
     color: Color,
     area: Rectangle
@@ -17,15 +18,20 @@ export class OneDrawStrategy implements DrawStrategy {
     );
   }
 
-  drawSprite(
+  public drawSprite(
     ctx: CanvasRenderingContext2D,
     sprite: Sprite,
     area: Rectangle
   ): void {
+    const scale = new Vector2(
+      area.getWidth() / sprite.getImageFrameWidth(),
+      area.getHeight() / sprite.getImageFrameHeight(),
+    );
+
     sprite.draw(
       ctx,
       area.getCoordinates(),
-      area.getDimensions()
+      scale
     );
   }
 
