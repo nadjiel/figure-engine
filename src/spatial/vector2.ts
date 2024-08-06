22252582337dc565a9f1a6aac3b19a1835c3273c
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2024-08-06 10:45:16 -0300

    feat: implementing some methods in Vector2

--- src/spatial/vector2.ts
+++ src/spatial/vector2.ts
@@ -17,6 +17,36 @@ export class Vector2 extends VectorN {
   }
 
   /**
+   * This method returns a normalized vector pointing up.
+   * (Considering that the y axis goes up negatively)
+   */
+  public static createUp(): Vector2 {
+    return new Vector2(0, -1);
+  }
+
+  /**
+   * This method returns a normalized vector pointing up.
+   */
+  public static createRight(): Vector2 {
+    return new Vector2(1, 0);
+  }
+
+  /**
+   * This method returns a normalized vector pointing down.
+   * (Considering that the y axis goes down positively)
+   */
+  public static createDown(): Vector2 {
+    return new Vector2(0, 1);
+  }
+
+  /**
+   * This method returns a normalized vector pointing up.
+   */
+  public static createLeft(): Vector2 {
+    return new Vector2(-1, 0);
+  }
+
+  /**
    * @param x The x component of the vector
    * @param y The y component of the vector
    */
@@ -38,6 +68,34 @@ export class Vector2 extends VectorN {
 
   public getY(): number {
     return this.getComponent(1);
+  }
+
+  public plus(vector: Vector2): Vector2 {
+    return super.plus(vector) as Vector2;
+  }
+
+  public minus(vector: Vector2): Vector2 {
+    return super.minus(vector) as Vector2;
+  }
+
+  public incrementBy(value: number): Vector2 {
+    return super.incrementBy(value) as Vector2;
+  }
+
+  public decrementBy(value: number): Vector2 {
+    return super.decrementBy(value) as Vector2;
+  }
+
+  public increment(): Vector2 {
+    return super.increment() as Vector2;
+  }
+
+  public decrement(): Vector2 {
+    return super.decrement() as Vector2;
+  }
+
+  public scaleBy(scalar: number): Vector2 {
+    return super.scaleBy(scalar) as Vector2;
   }
 
 }
