a127e3807318bb99ff79bad8f154cbf3a46201cd
Daniel de Oliveira <oliveira.daaaaniel@gmail.com>
2024-08-29 10:28:44 -0300

    refac: erasing unecessary method overriding

--- src/stage/camera/staticCamera.ts
+++ src/stage/camera/staticCamera.ts
@@ -2,22 +2,6 @@ import { Camera } from "./camera.js";
 
 export class StaticCamera extends Camera {
 
-  public start(): void {
-    
-  }
-
-  public update(): void {
-    
-  }
-
-  public draw(ctx: CanvasRenderingContext2D): void {
-    
-  }
-
-  public stop(): void {
-    
-  }
-
   public onStart(): void {
     
   }
